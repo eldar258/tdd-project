89fd668418187fb0d47bee0211ffabb48ef95b19
Eldar <9eldik7@gmail.com>
2023-07-05 15:20:15 +0300

    feat: conversion of Money from EUR to USD

--- stocks/portfolio.go
+++ stocks/portfolio.go
@@ -9,7 +9,15 @@ func (p Portfolio) Add(m Money) Portfolio {
 func (p Portfolio) Evaluate(currency string) Money {
 	total := 0.0
 	for _, m := range p {
-		total += m.amount
+		total += convert(m, currency)
 	}
 	return Money{amount: total, currency: currency}
+}
+
+func convert(money Money, currency string) float64 {
+	eurToUsd := 1.2
+	if currency == money.currency {
+		return money.amount
+	}
+	return money.amount * eurToUsd
 }
